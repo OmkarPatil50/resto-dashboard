130ebd5732da709189ccf8911c6336d27660cd8b
Omkar Patil <Omkarp5429@gmail.com>
2023-12-02 00:23:07 +0530

    Added: Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import DashBoard from './pages/DashBoard/DashBoard';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-     Hello
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/dashboard' element={<DashBoard />} />
+      </Routes>
     </div>
   );
 }

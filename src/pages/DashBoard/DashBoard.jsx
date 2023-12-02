dd0ed5b1fa464a4b652666cc112d5ebc2466fd0e
Omkar Patil <Omkarp5429@gmail.com>
2023-12-02 22:42:59 +0530

    Updated: Dashboard

--- src/pages/DashBoard/DashBoard.jsx
+++ src/pages/DashBoard/DashBoard.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import BarChart from "../../components/BarChart";
+import "./DashBoard.css";
 
 const DashBoard = () => {
   const previousLocationData = useLocation();
@@ -74,176 +76,137 @@ const DashBoard = () => {
   } = adminDetails;
 
   return (
-    <div>
-      {adminDetails && (
-        <>
-          <h1>
-            {name}, {location} on Dhun Jam
-          </h1>
-          <section>
-            <div>
-              <p>Do you want to charge your customers for requesting songs?</p>
-              <div>
-                <label htmlFor="radio-yes">
-                  <input
-                    type="radio"
-                    name="radio"
-                    id="radio-yes"
-                    checked={charge_customers === true}
-                    onChange={() => {
-                      setAdminDetails((prev) => {
-                        return { ...prev, charge_customers: true };
-                      });
-                    }}
-                  />{" "}
-                  Yes
-                </label>
-                <label htmlFor="radio-no">
-                  <input
-                    type="radio"
-                    name="radio"
-                    id="radio-no"
-                    checked={charge_customers === false}
-                    onChange={() => {
-                      setAdminDetails((prev) => {
-                        return { ...prev, charge_customers: false };
-                      });
-                    }}
-                  />{" "}
-                  No
-                </label>
-              </div>
-            </div>
-            <div>
-              <p>Custom song request amount-</p>
-              <label htmlFor="song-charges">
-                <input
-                  type="number"
-                  id="song-charges"
-                  disabled={charge_customers === false}
-                  required={charge_customers ? true : false}
-                  value={category_6}
-                  min="99"
-                  onChange={(event) => {
-                    setAdminDetails((prev) => {
-                      return {
-                        ...prev,
-                        amount: {
-                          ...prev.amount,
-                          category_6: event.target.value,
-                        },
-                      };
-                    });
-                  }}
-                />
-              </label>
-            </div>
-            <div>
-              <p>Regular song request amounts, from high to low-</p>
-              <label htmlFor="regular-song-charges-1">
-                <input
-                  type="number"
-                  id="regular-song-charges-1"
-                  disabled={charge_customers === false}
-                  required={charge_customers ? true : false}
-                  value={category_7}
-                  min="79"
-                  onChange={(event) => {
-                    setAdminDetails((prev) => {
-                      return {
-                        ...prev,
-                        amount: {
-                          ...prev.amount,
-                          category_7: event.target.value,
-                        },
-                      };
-                    });
-                  }}
-                />
-              </label>
-              <label htmlFor="regular-song-charges-2">
-                <input
-                  type="number"
-                  id="regular-song-charges-2"
-                  disabled={charge_customers === false}
-                  required={charge_customers ? true : false}
-                  value={category_8}
-                  min="59"
-                  onChange={(event) => {
-                    setAdminDetails((prev) => {
-                      return {
-                        ...prev,
-                        amount: {
-                          ...prev.amount,
-                          category_8: event.target.value,
-                        },
-                      };
-                    });
-                  }}
-                />
-              </label>
-              <label htmlFor="regular-song-charges-3">
-                <input
-                  type="number"
-                  id="regular-song-charges-3"
-                  disabled={charge_customers === false}
-                  required={charge_customers ? true : false}
-                  value={category_9}
-                  min="39"
-                  onChange={(event) => {
-                    setAdminDetails((prev) => {
-                      return {
-                        ...prev,
-                        amount: {
-                          ...prev.amount,
-                          category_9: event.target.value,
-                        },
-                      };
-                    });
-                  }}
-                />
-              </label>
-              <label htmlFor="regular-song-charges-4">
-                <input
-                  type="number"
-                  id="regular-song-charges-4"
-                  disabled={charge_customers === false}
-                  required={charge_customers ? true : false}
-                  value={category_10}
-                  min="19"
-                  onChange={(event) => {
-                    setAdminDetails((prev) => {
-                      return {
-                        ...prev,
-                        amount: {
-                          ...prev.amount,
-                          category_10: event.target.value,
-                        },
-                      };
-                    });
-                  }}
-                />
-              </label>
-            </div>
+    <div className="page flex-center">
+      <div className="dashboard-container flex-center flex-col">
+        {adminDetails && (
+          <>
+            <h1 className="dashboard-title">
+              {name}, {location} on Dhun Jam
+            </h1>
+            <section className="dashboard-section ">
+              <section className="questions-section grid-layout">
+                <div className="charge-customers-section grid">
+                  <p>
+                    Do you want to charge your customers for requesting songs?
+                  </p>
+                  <div className="radio-buttons flex">
+                    <label htmlFor="radio-yes" className="radio-label">
+                      <input
+                        className="radio-input"
+                        type="radio"
+                        name="radio"
+                        id="radio-yes"
+                        checked={charge_customers === true}
+                        onChange={() => {
+                          setAdminDetails((prev) => {
+                            return { ...prev, charge_customers: true };
+                          });
+                        }}
+                      />{" "}
+                      Yes
+                    </label>
+                    <label htmlFor="radio-no" className="radio-label">
+                      <input
+                        type="radio"
+                        name="radio"
+                        id="radio-no"
+                        className="radio-input"
+                        checked={charge_customers === false}
+                        onChange={() => {
+                          setAdminDetails((prev) => {
+                            return { ...prev, charge_customers: false };
+                          });
+                        }}
+                      />{" "}
+                      No
+                    </label>
+                  </div>
+                </div>
+                <div className="song-charges-section grid">
+                  <p>Custom song request amount-</p>
+                  <label htmlFor="song-charges" className="input-label">
+                    <input
+                      type="number"
+                      id="song-charges"
+                      disabled={charge_customers === false}
+                      required={charge_customers ? true : false}
+                      value={category_6}
+                      min="99"
+                      onChange={(event) => {
+                        setAdminDetails((prev) => {
+                          return {
+                            ...prev,
+                            amount: {
+                              ...prev.amount,
+                              category_6: event.target.value,
+                            },
+                          };
+                        });
+                      }}
+                      className="input-field-dashboard"
+                    />
+                  </label>
+                </div>
+                <div className="regular-song-charges-section grid">
+                  <p>Regular song request amounts, from high to low-</p>
+                  <ul className="grid-four">
+                    {[7, 8, 9, 10].map((category) => (
+                      <label
+                        key={`regular-song-charges-${category}`}
+                        htmlFor={`regular-song-charges-${category}`}
+                        className="input-label"
+                      >
+                        <input
+                          type="number"
+                          id={`regular-song-charges-${category}`}
+                          disabled={charge_customers === false}
+                          required={charge_customers ? true : false}
+                          value={adminDetails.amount[`category_${category}`]}
+                          min={(11 - category) * 20}
+                          onChange={(event) => {
+                            setAdminDetails((prev) => {
+                              return {
+                                ...prev,
+                                amount: {
+                                  ...prev.amount,
+                                  [`category_${category}`]: Number(
+                                    event.target.value
+                                  ),
+                                },
+                              };
+                            });
+                          }}
+                          className="input-field-dashboard"
+                        />
+                      </label>
+                    ))}
+                  </ul>
+                </div>
+              </section>
+              {charge_customers && (
+                <div className="bar-chart">
+                  <BarChart data={adminDetails.amount} className="barchart" />
+                </div>
+              )}
 
-         
-
-            <button
-              disabled={
-                charge_customers === false ||
-                category_6 < 99 ||
-                category_7 < 79 ||
-                category_8 < 59 ||
-                category_9 < 39 ||
-                category_10 < 19
-              }
-              onClick={updateData}
-            >
-              Save
-            </button>
-          </section>
-        </>
-      )}
+              <button
+                disabled={
+                  charge_customers === false ||
+                  category_6 < 99 ||
+                  category_7 < 79 ||
+                  category_8 < 59 ||
+                  category_9 < 39 ||
+                  category_10 < 19
+                }
+                onClick={updateData}
+                className="save-button"
+              >
+                Save
+              </button>
+            </section>
+          </>
+        )}
+      </div>
     </div>
   );
 };
